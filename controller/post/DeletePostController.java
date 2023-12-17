244cceafa5edd272ef196642e4b3c11eced6c74b
msjr1229 <147119104+msjr1229@users.noreply.github.com>
2023-12-17 16:32:15 +0900

    Update DeletePostController.java

--- controller/post/DeletePostController.java
+++ controller/post/DeletePostController.java
@@ -1,1 +1,16 @@
+package controller.post;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+
+public class DeletePostController implements Controller {
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
+    	
+    	return ""; 
+    }
+
+}
