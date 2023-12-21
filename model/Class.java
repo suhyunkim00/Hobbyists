60981f3cf7e223b95fc66cb106ee4836db190426
sjchae <121273119+SooJeongChae@users.noreply.github.com>
2023-12-21 19:32:27 +0900

    Class.java

--- model/Class.java
+++ model/Class.java
@@ -3,18 +3,23 @@ package model;
 import java.util.List;
 
 public class Class {
+	private String location;
 	private String title;
 	private String photo;
 	private String content;
 	private String uploader;
 	private int view;
 	private int id;
+	private int cateId;
+	private String cateName;
+	private int numOfMembers;
+	private List<User> memberList;
 
 	public Class() {
         // Default constructor
     }
 
-    public Class(String loation, String title, String photo, String content, 
+    public Class(String location, String title, String photo, String content, 
     				String  uploader, int view, int id) {
         super();
         this.location = location;
@@ -26,7 +31,22 @@ public class Class {
         this.id = id;
     }    
     
-	private String location;
+	public Class(String location, String title, String photo, String content, String uploader, int view, int id,
+			int cateId, String cateName, int numOfMembers, List<User> memberList) {
+		super();
+		this.location = location;
+		this.title = title;
+		this.photo = photo;
+		this.content = content;
+		this.uploader = uploader;
+		this.view = view;
+		this.id = id;
+		this.cateId = cateId;
+		this.cateName = cateName;
+		this.numOfMembers = numOfMembers;
+		this.memberList = memberList;
+	}
+
 	public String getLocation() {
 		return location;
 	}
@@ -82,4 +102,44 @@ public class Class {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	public int getCateId() {
+		return cateId;
+	}
+
+	public void setCateId(int cateId) {
+		this.cateId = cateId;
+	}
+
+	public String getCateName() {
+		return cateName;
+	}
+
+	public void setCateName(String cateName) {
+		this.cateName = cateName;
+	}
+
+	public int getNumOfMembers() {
+		return numOfMembers;
+	}
+
+	public void setNumOfMembers(int numOfMembers) {
+		this.numOfMembers = numOfMembers;
+	}
+
+	public List<User> getMemberList() {
+		return memberList;
+	}
+
+	public void setMemberList(List<User> memberList) {
+		this.memberList = memberList;
+	}
+
+	@Override
+	public String toString() {
+		return "Class [location=" + location + ", title=" + title + ", photo=" + photo + ", content=" + content
+				+ ", uploader=" + uploader + ", view=" + view + ", id=" + id + ", cateId=" + cateId + ", cateName="
+				+ cateName + ", numOfMembers=" + numOfMembers + ", memberList=" + memberList + "]";
+	}
+
 }
