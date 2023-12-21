e3dc0bcb0a78526e7b549a9baf83c2c0852314f0
sjchae <121273119+SooJeongChae@users.noreply.github.com>
2023-12-21 19:25:44 +0900

    Add files via upload

---
+++ model/service/ExistingClassException.java
@@ -0,0 +1,13 @@
+package model.service;
+
+public class ExistingClassException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ExistingClassException() {
+		super();
+	}
+
+	public ExistingClassException(String arg0) {
+		super(arg0);
+	}
+}
